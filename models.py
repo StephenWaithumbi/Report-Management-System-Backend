51d250408d705eaca29d33955bfb7e08eb9fe8f0
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2025-03-21 16:06:34 +0300

    changes in models

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='department_user')
 
